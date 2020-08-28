ded8b04765f24d759881693b94f3a37edd1d17ed
Jithin M <jithinmbalu14@gmail.com>
2020-08-28 21:43:58 +0530

    Created xcode project and setting up remote Git project

---
+++ AirBnbAssignment/AirBnbAssignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AirBnbAssignment
+//
+//  Created by Jithin M on 28/08/20.
+//  Copyright © 2020 Jithin M. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
